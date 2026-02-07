8afca02b5899951ce48ad0cef20a3e923b2c023f
TeruyaY <terryinwaseda@ruri.waseda.jp>
2026-02-07 16:22:19 +0900

    Finish Adding produts to backend

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -6,6 +6,7 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 import {ProductProvider} from './ProductContext.jsx'
 import ProductsTable from './components/ProductsTable.jsx'
+import AddProducts from './components/AddProducts.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,9 @@ function App() {
               </div>
             
               } /> 
+
+            <Route path="/addproduct" element={
+              <AddProducts />} />
           </Routes>
         </ProductProvider>
       </Router>
